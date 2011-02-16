90b199c77408868f6c761285fb7ef6888a427bb9
christian.futterlieb <christian.futterlieb@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-02-16 15:46:45 +0000

    Updated version to 0.2.0 Updated EMConf
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pagenotfoundehandling/trunk@43709 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "pagenotfoundhandling".
 #
-# Auto generated 25-11-2010 17:41
+# Auto generated 16-02-2011 16:45
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => 'Agentur am Wasser | Maeder & Partner AG',
-	'version' => '0.1.0',
+	'version' => '0.2.0',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'static_info_tables' => '',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"2daa";s:10:"README.txt";s:4:"42ba";s:33:"class.tx_pagenotfoundhandling.php";s:4:"21a1";s:48:"class.tx_pagenotfoundhandling_LanguageSelect.php";s:4:"e6a0";s:21:"ext_conf_template.txt";s:4:"b3c2";s:12:"ext_icon.gif";s:4:"9d5d";s:17:"ext_localconf.php";s:4:"da82";s:14:"ext_tables.php";s:4:"2fc0";s:14:"ext_tables.sql";s:4:"c182";s:17:"locallang_404.xml";s:4:"f263";s:16:"locallang_db.xml";s:4:"de7a";s:14:"doc/manual.sxw";s:4:"9555";s:24:"res/defaultTemplate.tmpl";s:4:"7f5e";}',
+	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"1da7";s:10:"README.txt";s:4:"42ba";s:33:"class.tx_pagenotfoundhandling.php";s:4:"22c2";s:48:"class.tx_pagenotfoundhandling_LanguageSelect.php";s:4:"e6a0";s:21:"ext_conf_template.txt";s:4:"dd0e";s:12:"ext_icon.gif";s:4:"9d5d";s:17:"ext_localconf.php";s:4:"da82";s:14:"ext_tables.php";s:4:"cc94";s:14:"ext_tables.sql";s:4:"1448";s:17:"locallang_404.xml";s:4:"fd81";s:16:"locallang_db.xml";s:4:"601d";s:14:"doc/manual.sxw";s:4:"c0e7";s:24:"res/defaultTemplate.tmpl";s:4:"c28a";}',
 	'suggests' => array(
 	),
 );
