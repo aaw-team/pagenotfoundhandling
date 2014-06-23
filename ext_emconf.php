935ca3679ed5d17399960c98131287e9b0655d9e
Christian Futterlieb <christian@futterlieb.ch>
2014-06-23 14:13:23 +0200

    - releasing version 2.0.0 to TER (it should have been 1.0.0, but I messed it up..)

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "pagenotfoundhandling".
  *
- * Auto generated 07-04-2014 18:51
+ * Auto generated 23-06-2014 13:55
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => 'Agentur am Wasser | Maeder & Partner AG',
-	'version' => '1.0.0',
+	'version' => '2.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5.0-6.2.99',
@@ -37,10 +37,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'conflicts' => array(
 		),
 		'suggests' => array(
-			//'static_info_tables' => '',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"2d5b";s:33:"class.tx_pagenotfoundhandling.php";s:4:"f05f";s:48:"class.tx_pagenotfoundhandling_LanguageSelect.php";s:4:"2872";s:21:"ext_conf_template.txt";s:4:"5a0c";s:12:"ext_icon.gif";s:4:"9d5d";s:17:"ext_localconf.php";s:4:"da82";s:14:"ext_tables.php";s:4:"e4f2";s:14:"ext_tables.sql";s:4:"aaab";s:17:"locallang_404.xml";s:4:"fd81";s:16:"locallang_db.xml";s:4:"2b2b";s:10:"README.txt";s:4:"42ba";s:14:"doc/manual.sxw";s:4:"79ca";s:24:"res/defaultTemplate.tmpl";s:4:"c28a";}',
+	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"c1dc";s:33:"class.tx_pagenotfoundhandling.php";s:4:"e433";s:48:"class.tx_pagenotfoundhandling_LanguageSelect.php";s:4:"0cf6";s:16:"ext_autoload.php";s:4:"307a";s:21:"ext_conf_template.txt";s:4:"9f4e";s:12:"ext_icon.gif";s:4:"9d5d";s:17:"ext_localconf.php";s:4:"2664";s:14:"ext_tables.php";s:4:"d3f7";s:14:"ext_tables.sql";s:4:"37d9";s:17:"locallang_404.xml";s:4:"fd81";s:16:"locallang_db.xml";s:4:"de70";s:10:"README.txt";s:4:"407e";s:45:"Classes/Controller/PagenotfoundController.php";s:4:"9eef";s:35:"Classes/Utility/LanguageUtility.php";s:4:"6770";s:44:"Configuration/TCA/Overrides/TcaAdditions.php";s:4:"5d0c";s:40:"Resources/Private/Templates/default.html";s:4:"de6b";s:14:"doc/manual.sxw";s:4:"79ca";s:24:"res/defaultTemplate.tmpl";s:4:"c28a";}',
 	'suggests' => array(
 	),
 );
