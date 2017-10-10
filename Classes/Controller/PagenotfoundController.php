aaw-team/pagenotfoundhandling

commit 17db1f217c63b7533fd0803407f52d67364b9be5

[BUGFIX] Fix Basic Auth in TYPO3 >8.1
Use RequestFactory when an authorization is part of the game,
because passing the HTTP header to GeneralUtility::getURL() won't
be accepted by GuzzleHttp backend.

Resolves: #1

Classes/Controller/PagenotfoundController.php
@@ -557,6 +557,7 @@ class PagenotfoundController
     {
         // handle http authorization
         $digestAuthorization = false;
+        $basicAuthorization = '';
         $requestHeaders = $this->_getAllHttpHeaders();
         if (array_key_exists('Authorization', $requestHeaders)) {
             $authorizationHeader = $requestHeaders['Authorization'];
@@ -564,17 +565,79 @@ class PagenotfoundController
             if (strpos($authorizationHeader, 'Basic ') === 0) {
                 // check the header value for authentication basic,
                 // only base64 characters are allowed
-                if (preg_match('~[^a-zA-Z0-9+/=]~', substr($authorizationHeader, 6)) === 0) {
-                    $headers[] = 'Authorization: ' . $authorizationHeader;
+                $basicAuthorization = substr($authorizationHeader, 6);
+                if (preg_match('~[^a-zA-Z0-9+/=]~', $basicAuthorization) !== 0) {
+                    $basicAuthorization = '';
                 }
             } elseif (strpos($authorizationHeader, 'Digest ') === 0) {
                 $digestAuthorization = true;
             }
         }
 
-        if ($digestAuthorization === true) {
+        // Use RequestFactory in TYPO3 >= 8.1
+        if ((!empty($basicAuthorization) || $digestAuthorization) && version_compare(TYPO3_version, '8.1', '>=')) {
+            // Setup options for the request
+            $options = [
+                \GuzzleHttp\RequestOptions::HEADERS => $headers,
+            ];
+
+            // Handle authorization
+            if (!empty($basicAuthorization)) {
+                list ($username, $password) = explode(':', base64_decode($basicAuthorization), 2);
+                $options[\GuzzleHttp\RequestOptions::AUTH] = [$username, $password];
+            } elseif ($digestAuthorization) {
+                list ($username, $password) = GeneralUtility::trimExplode(':', $this->_digestAuthentication, false, 2);
+                $options[\GuzzleHttp\RequestOptions::AUTH] = [$username, $password, 'digest'];
+            }
+
+            /** @var \TYPO3\CMS\Core\Http\RequestFactory $requestFactory */
+            $requestFactory = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Http\RequestFactory::class);
+            try {
+                $response = $requestFactory->request($url, 'GET', $options);
+                $return = '';
+
+                // Do it almost the same way as in GeneralUtility::getUrl()
+                $includeHeaders = (int)$includeHeaders;
+                if ($includeHeaders) {
+                    foreach ($response->getHeaders() as $name => $values) {
+                        $return .= $name . ": " . implode(", ", $values) . CRLF;
+                    }
+                    $return .= CRLF;
+                }
+                if ($includeHeaders !== 2) {
+                    $return .= $response->getBody()->getContents();
+                }
+                if (isset($report)) {
+                    $report['lib'] = 'http';
+                    if ($response->getStatusCode() >= 300 && $response->getStatusCode() < 400) {
+                        $report['http_code'] = $response->getStatusCode();
+                        $report['content_type'] = $response->getHeader('Content-Type');
+                        $report['error'] = $response->getStatusCode();
+                        $report['message'] = $response->getReasonPhrase();
+                    } elseif (!empty($return)) {
+                        $report['error'] = $response->getStatusCode();
+                        $report['message'] = $response->getReasonPhrase();
+                    } elseif ($includeHeaders) {
+                        // Set only for $includeHeader to work exactly like PHP variant
+                        $report['http_code'] = $response->getStatusCode();
+                        $report['content_type'] = $response->getHeader('Content-Type');
+                    }
+                }
+            } catch (\GuzzleHttp\Exception\RequestException $e) {
+                $return = false;
+                if (isset($report)) {
+                    $report['error'] = $e->getCode();
+                    $report['message'] = $e->getMessage();
+                }
+            }
+        } elseif ($digestAuthorization) {
+            // Digest authorization in TYPO3 < 8.1
             $return = $this->_getUrlWithDigestAuthentication($url, $includeHeaders, $headers, $report);
         } else {
+            // Default
+            if (!empty($basicAuthorization)) {
+                $headers[] = 'Authorization: Basic ' . $basicAuthorization;
+            }
             $return = GeneralUtility::getURL($url, $includeHeaders, $headers, $report);
         }
 

commit be0de4dd6442ab45706a81cf1ba2efa36b3a7aa1

[BUGFIX] Always use RequestFactory in TYPO3 >= 8.1
Regression bug introduced in
17db1f217c63b7533fd0803407f52d67364b9be5 and not completely fixed
with 5b9c6b95bc410ee9e5dd3f228967227718ba4af3

Classes/Controller/PagenotfoundController.php
@@ -586,7 +586,7 @@ class PagenotfoundController
         }
 
         // Use RequestFactory in TYPO3 >= 8.1
-        if ((!empty($basicAuthorization) || $digestAuthorization) && version_compare(TYPO3_version, '8.1', '>=')) {
+        if (version_compare(TYPO3_version, '8.1', '>=')) {
             // Setup options for the request
             $options = [
                 \GuzzleHttp\RequestOptions::HEADERS => [],
@@ -653,7 +653,7 @@ class PagenotfoundController
             // Digest authorization in TYPO3 < 8.1
             $return = $this->_getUrlWithDigestAuthentication($url, $includeHeaders, $headers, $report);
         } else {
-            // Default
+            // Default for TYPO3 < 8.1
             if (!empty($basicAuthorization)) {
                 $headers[] = 'Authorization: Basic ' . $basicAuthorization;
             }