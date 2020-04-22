7419a5be0319136ab4c61fe637462444d02a28c3
Christian Futterlieb <christian@futterlieb.ch>
2020-04-22 14:51:27 +0200

    [BUGFIX] Correct TYPO3 version dependency constraint
    Follow-up to: 10634c964e4ed3f7763bfd4ce70772ad5c341229

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,7 +26,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '7.2',
-            'typo3' => '9.5.99-10.3.99',
+            'typo3' => '9.5.0-10.3.99',
         ],
         'conflicts' => [],
         'suggests' => [],
